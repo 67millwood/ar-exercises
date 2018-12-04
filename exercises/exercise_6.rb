90dcc27f2a3b6134f0f46dc41a863063d922a8a7
67millwood <info@track2development.com>
2018-12-04 18:26:11 +0000

    exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tiger", last_name: "Woods", hourly_rate: 55)
+@store1.employees.create(first_name: "Justin", last_name: "Thomas", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Mike", last_name: "Judge", hourly_rate: 25)
+@store2.employees.create(first_name: "Jagdeep", last_name: "Singh", hourly_rate: 15)
+
+puts Employee.count
+puts @store1.employees.count
