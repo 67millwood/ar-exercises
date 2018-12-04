55947da857e0b6385e6f8f68ff66282834b5c67d
67millwood <info@track2development.com>
2018-12-04 16:31:11 +0000

    exercise 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+puts Store.first.name
+@store1 = Store.first
+puts "I've assigned it to a variable #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+puts @store2.name
+@store1.name = "Toronto"
+puts @store1.name
